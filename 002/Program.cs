e2401b0a0e933b2c4576aaee34126320d8adacf3
«Vova <vrobskiy@gmail.com>
2022-04-28 21:51:00 +0300

    Example 002 masstives

---
+++ 002/Program.cs
@@ -0,0 +1,66 @@
+﻿//Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+/*Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] Fibonacci = new int [length];
+Fibonacci[0] = 0;
+Fibonacci[1] = 1;
+for (int i = 2; i < length; i++)
+{
+    Fibonacci[i] = Fibonacci[i-1] + Fibonacci[i-2];
+}
+WriteArray(Fibonacci);
+ 
+ 
+ 
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " "); 
+    }
+    Console.WriteLine();
+}*/
+
+
+
+
+// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+
+Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] Fibonacci = new int [length];
+
+
+FillArray(Fibonacci);
+WriteArray(Fibonacci);
+
+int [] newArray = new int [length];
+
+for (int i = 0; i < length; i++)
+{
+    newArray[i] = Fibonacci[i];
+}
+Console.Write("Копия массива: ");
+WriteArray(newArray);
+
+
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " "); 
+    }
+    Console.WriteLine();
+}
